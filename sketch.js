f280b881460a945e22b1aa7a5b8ede6d932d3235
Priyankap-004 <70195388+Priyankap-004@users.noreply.github.com>
2020-10-23 16:23:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+  
+ 
+ 
+  drawSprites();
+ 
+}
+
+
+
